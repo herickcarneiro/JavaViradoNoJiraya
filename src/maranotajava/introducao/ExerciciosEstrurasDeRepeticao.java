3562aeeee3c2cbd0b20c270e8b6477d03ed6e279
Herick Carneiro <herickcarneiro@gmail.com>
2025-03-17 23:01:32 -0300

    Create ExerciciosEstrurasDeRepeticao.java

---
+++ src/maranotajava/introducao/ExerciciosEstrurasDeRepeticao.java
@@ -0,0 +1,12 @@
+package maranotajava.introducao;
+
+public class ExerciciosEstrurasDeRepeticao {
+    public static void main(String[] args) {
+        // Imprimir todos os numeros pares de 0 até 100000
+        for( int i = 0; i <= 1000000; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
