821de1b4692a86237bb0d4b91fe60aa5ff3430a2
Herick Carneiro <herickcarneiro@gmail.com>
2025-03-17 23:01:40 -0300

    Create EstruturasDeRepetição.java

---
+++ src/maranotajava/introducao/EstruturasDeRepetição.java
@@ -0,0 +1,15 @@
+package maranotajava.introducao;
+
+public class EstruturasDeRepetição {
+    public static void main(String[] args) {
+        // while, do-while, for
+        int count = 12;
+        while(count < 10){
+            System.out.println(++count);
+        }
+        do{
+            System.out.println(++count);
+        }while(count < 10);
+
+    }
+}
